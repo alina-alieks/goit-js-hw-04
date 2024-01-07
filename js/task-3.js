4b55b3c7a26ffd77509d4b4965b98992a591880d
alina-alieks <145293161+alina-alieks@users.noreply.github.com>
2024-01-07 14:50:52 +0200

    hw done

--- js/task-3.js
+++ js/task-3.js
@@ -1,20 +1,26 @@
 "use strict"
 
-function getElementWidth(content, padding, border) {
-    //2 ways resolving the task
-    //1-st
-    const boxSizing = Number.parseFloat(content) + 2 * Number.parseFloat(padding) + 2 * Number.parseFloat(border);
-    
-    //2-d
-    // content = Number.parseFloat(content);
-    // padding = Number.parseFloat(padding);
-    // border = Number.parseFloat(border);
-    // const boxSizing = content + 2 * padding + 2 * border;
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+    changeUsername(newName) {
+        this.username = newName;
+    },
+    updatePlayTime(hours) {
+        this.playTime += hours;
+    },
+    getInfo() {
+        return `${this.username} has ${this.playTime} active hours`;
+    },
+};
 
-    return boxSizing;
 
-}
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
